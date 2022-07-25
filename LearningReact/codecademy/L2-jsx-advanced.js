58d8c661c9b50146f7a61eb48e41818473b5fd5a
Umberto Pasinetti <pasinetti96@gmail.com>
2022-07-25 20:41:57 +0200

    up

--- LearningReact/codecademy/L2-jsx-advanced.js
+++ LearningReact/codecademy/L2-jsx-advanced.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // 1. class vs className
+// in JSX NO class, YES className
+// class: reserved word in JS
+// (it is translated by the compiler in class)
+const myDiv = <div className="big">I AM A BIG DIV</div>;
+
+ReactDOM.render(myDiv, document.getElementById('app'));
