c4c30f13a145a45ff60e3985e32986518ffcf049
Umberto Pasinetti <pasinetti96@gmail.com>
2023-07-10 18:50:25 +0200

    first steps with typescript

---
+++ TypeScript/lessons/app.js
@@ -0,0 +1,5 @@
+// L3
+var sum = function (n1, n2) {
+    return n1 + n2;
+};
+console.log(sum(3, 5));
---
+++ TypeScript/lessons/app.ts
@@ -0,0 +1,6 @@
+// L3
+const sum = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+console.log(sum(3, 5));
